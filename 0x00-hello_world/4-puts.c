6340b55806760bc44bf7531eed96569f9568d910
Jhofix <dadajoshua518@gmail.com>
2022-06-03 06:54:32 -0700

    hello world task 5a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * main -  outputs \"Programming is like building a multilingual puzzle
+ *
+ * Description: Displays main's content
+ *
+ * Return: Always return 0
+ */
+#include < stdio.h >
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ * main - Short description
+ *
+ * Description: The main function outputs the line of text
+ *
+ * Return: Always return 0
+ */
+#include < stdio.h >
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
