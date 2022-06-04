80d37b36c8f1ab73ded8193b3b7f29f112b188b9
Jhofix <dadajoshua518@gmail.com>
2022-06-03 21:23:29 -0700

    C variables commit 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include < stdio.h >
+
+/**
+ * main - print a to z
+ *
+ * Description: Uses only putchar
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar("%c\n", n);
+	return (0);
+}
