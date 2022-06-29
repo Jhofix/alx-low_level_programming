d82225f4f75b8f3a12285218b4b73eccd1c44ef0
Jhofix <dadajoshua518@gmail.com>
2022-06-29 08:20:27 -0700

    recursion 6a

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - involke _sqrt
+ * @n: target value
+ *
+ * Return: integer value
+ */
+
+int is_prime_number(int n)
+{
+	return (_primetest(n, 2));
+}
+
+/**
+ * _primetest - test function
+ * @n: target value
+ * @a: test value
+ * 
+ * Return: 0 or 1
+ */
+
+int _primetest(int n, int a)
+{
+	
+	if ((a < n) && (n % 5 != 0)&& (n % 3 != 0) && (n % a != 0))
+	{
+		return (1);
+		_primetest(n, (a + 1));
+	
+	}
+
+	else
+	{
+		return (0);
+	}
+		
+}
