dfcfcf00adb741039d9a40053c99da5a22b2e972
Jhofix <dadajoshua518@gmail.com>
2022-06-29 21:06:18 -0700

    argc 2a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ *Return: 0
+ */
+ 
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	
+	return (0);
+}
