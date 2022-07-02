ed18381963acd8dfbd313af9ca292c67cc1ac40d
Jhofix <dadajoshua518@gmail.com>
2022-07-01 21:09:55 -0700

    malloc 2a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concat string
+ * @s1: first character
+ * @s2: second character
+ *
+ * Return: strcat
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+	char *strcat;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	strcat = malloc(sizeof(s1) * j + i + 1);
+	if (strcat == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		strcat[k] = s1[k];
+	}
+	for (; k < i + j + 1; k++)
+	{
+		strcat[k] = s2[k - i];
+	}
+	return (strcat);
+}
