65cadd0185f90d1d35557e0544a09472e309bc89
Jhofix <dadajoshua518@gmail.com>
2022-07-05 12:18:57 -0700

    structures 1a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h
+
+/**
+ * init_dog - structure definition
+ * @d: structure arguement
+ * @name: second arguement
+ * @age: third arguement
+ * @owner: fourth arguement
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
