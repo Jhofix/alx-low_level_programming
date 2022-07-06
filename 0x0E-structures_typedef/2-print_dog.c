221e671878969a422d3830dbb3ce342f43e8e571
Jhofix <dadajoshua518@gmail.com>
2022-07-05 21:40:43 -0700

    structures 2f

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,47 +1,24 @@
-#include <stdio.h>
 #include "dog.h"
-
+#include <stdio.h>
 
 /**
- * print_dog - structure definition
- * @d: structure arguement
+ * print_dog - prints a dog
  *
- * Description: description
+ * @d: dog to print
+ *
+ * Return: void
  */
-
 void print_dog(struct dog *d)
 {
-	char *dnm = d->name, *down = d->owner;
-	float dag = d->age;
-
 	if (d == 0)
-	{
 		return;
-	}
-	if (dnm == 0)
-	{
-		dnm = "(nil)";
-		printf("Name: %s\n", dnm);
-	}
+	if (d->name == 0)
+		printf("Name: (nil)\n");
 	else
-	{
-		printf("Name: %s\n", dnm);
-	}
-	if ((dag) == 0)
-	{
-		printf("Age: (nil)\n");
-	}
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == 0)
+		printf("Owner: (nil)\n");
 	else
-	{
-		printf("Age: %.1f\n", dag);
-	}
-	if (down == 0)
-	{
-		down = "(nil)";
-		printf("Owner: %s\n", down);
-	}
-	else
-	{
-		printf("Owner: %s\n", down);
-	}
+		printf("Owner: %s\n", d->owner);
 }
